c6a3c8b60935c3783f694451767465f894ec5bc8
-g <kaecio>
2022-11-26 22:13:03 -0300

    first commit

---
+++ src/main/java/br/com/dio/labpadroesprojetospring/service/impl/ClienteServiceImpl.java
@@ -0,0 +1,31 @@
+package br.com.dio.labpadroesprojetospring.service.impl;
+
+import br.com.dio.labpadroesprojetospring.model.Cliente;
+import br.com.dio.labpadroesprojetospring.service.ClienteService;
+
+public class ClienteServiceImpl implements ClienteService {
+    @Override
+    public Iterable<Cliente> buscarTodos() {
+        return null;
+    }
+
+    @Override
+    public Cliente buscarPorId(Long id) {
+        return null;
+    }
+
+    @Override
+    public void inserir(Cliente cliente) {
+
+    }
+
+    @Override
+    public void atualizar(Long id, Cliente cliente) {
+
+    }
+
+    @Override
+    public void deletar(Long id) {
+
+    }
+}
